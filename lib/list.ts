41247093ec28055d69d168bf95b70fdc0d08fee8
Jonathan Neidel <jneidel@jneidel.com>
2020-08-16 22:17:19 +0200

    Convert list to using ItemList

--- lib/list.ts
+++ lib/list.ts
@@ -1,23 +1,25 @@
 import { validateFilter } from "./validateFilter";
-import { readConfig } from "./fs";
+import ItemList from "./ItemList";
 
 export default async function list( unvalidatedFilter = null ) {
   const filter = validateFilter( unvalidatedFilter );
 
-  const data = await readConfig()
-    .then( d => filter ?  d.filter( d => d.type === filter ) : d )
-    .then( d => d.map( d => {
+  const list = new ItemList();
+  await list.reloadFromConfig();
+
+  list.addFilter( filter );
+  const data = list.get()
+    .map( d => {
       let res = d.name;
 
       if ( d.desc )
         res = `${res}: ${d.desc}`;
 
       if ( !filter )
-        res = `${res} [${d.type}]`;
+        res = `${res} [${d.medium}]`;
 
       return res;
-    } )
-  )
+    } );
 
   const dataToPrint = data.reduce( ( acc, cur ) => acc = `${acc}\n${cur}`, "" );
   console.log( dataToPrint );
